327887ecd3c3aa1e35791e0ded06a9ce99f11def
Lou Berger <lberger@labn.net>
2015-09-02 14:28:55 -0400

    DROP: rebase f*ckup

--- bgpd/bgp_aspath.c
+++ bgpd/bgp_aspath.c
@@ -203,6 +203,19 @@ assegment_prepend_asns (struct assegment *seg, as_t asnum, int num)
   if ((newas = assegment_data_new (seg->length + num)) == NULL)
     return seg;
   
+  if (newas)
+    {
+      int i;
+      for (i = 0; i < num; i++)
+        newas[i] = asnum;
+      
+      memcpy (newas + num, seg->as, ASSEGMENT_DATA_SIZE (seg->length, 1));
+      assegment_data_free (seg->as);
+      seg->as = newas; 
+      seg->length += num;
+      return seg;
+    }
+
   for (i = 0; i < num; i++)
     newas[i] = asnum;
 
