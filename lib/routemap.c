41cc2c087313e05f616b97d1abf95b5a12943e47
Lou Berger <lberger@labn.net>
2015-09-02 14:28:55 -0400

    Cleanup memory on exit, fix writemap config

--- lib/routemap.c
+++ lib/routemap.c
@@ -897,6 +897,9 @@ route_map_finish (void)
   route_match_vec = NULL;
   vector_free (route_set_vec);
   route_set_vec = NULL;
+  /* cleanup route_map */
+  while (route_map_master.head) 
+      route_map_delete (route_map_master.head);
 }
 
 /* VTY related functions. */
@@ -1285,6 +1288,8 @@ route_map_config_write (struct vty *vty)
 	
 	write++;
       }
+  if (!first)
+      vty_out (vty, " exit%s", VTY_NEWLINE);
   return write;
 }
 
