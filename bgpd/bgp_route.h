e2780ac2589b23d9b426837ad2f56b910bcc4203
Lou Berger <lberger@labn.net>
2015-09-04 09:22:42 -0400

    bgpd: make _vpnv4 static handling SAFI-agnostic
    This changes the existing _vpnv4 functions for MPLS-VPN into
    SAFI-agnostic functions, renaming them from *_vpnv4 to *_safi.
    
    Also adds route-map support while at it.
    
    Signed-off-by: Lou Berger <lberger@labn.net>
    Reviewed-by: David Lamparter <equinox@opensourcerouting.org>

--- bgpd/bgp_route.h
+++ bgpd/bgp_route.h
@@ -209,10 +209,10 @@ extern void bgp_static_update (struct bgp *, struct prefix *, struct bgp_static 
 			afi_t, safi_t);
 extern void bgp_static_withdraw (struct bgp *, struct prefix *, afi_t, safi_t);
                      
-extern int bgp_static_set_vpnv4 (struct vty *vty, const char *, 
-                          const char *, const char *);
+extern int bgp_static_set_safi (safi_t safi, struct vty *vty, const char *, 
+                          const char *, const char *, const char *);
 
-extern int bgp_static_unset_vpnv4 (struct vty *, const char *, 
+extern int bgp_static_unset_safi (safi_t safi, struct vty *, const char *, 
                             const char *, const char *);
 
 /* this is primarily for MPLS-VPN */
