292fee6b67729c8ad47b902d5c2d0f1e1a39b674
David Lamparter <equinox@opensourcerouting.org>
2015-12-18 18:12:43 +0100

    DROP: decode_rd_as4 snafu

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -78,12 +78,12 @@ decode_rd_as (u_char *pnt, struct rd_as *rd_as)
 static void
 decode_rd_as4 (u_char *pnt, struct rd_as *rd_as)
 {
-  rd_as->as  = (u_int32_t) *pnt++ << 24;
-  rd_as->as |= (u_int32_t) *pnt++ << 16;
-  rd_as->as |= (u_int32_t) *pnt++ << 8;
+  rd_as->as = (u_int32_t) *pnt++ << 24;
+  rd_as->as = (u_int32_t) *pnt++ << 16;
+  rd_as->as = (u_int32_t) *pnt++ << 8;
   rd_as->as |= (u_int32_t) *pnt++;
   
-  rd_as->val  = ((u_int16_t) *pnt++ << 8);
+  rd_as->val |= ((u_int16_t) *pnt++ << 8);
   rd_as->val |= (u_int16_t) *pnt;
 }
 
