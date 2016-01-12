fa64191969cae58745df78c7a1a867bc3a37f599
Lou Berger <lberger@labn.net>
2016-01-12 09:31:09 -0500

    bgpd: encap: remove duplicate encap attribute

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -2952,45 +2952,8 @@ bgp_packet_attribute (struct bgp *bgp, struct peer *peer,
   if ((afi == AFI_IP || afi == AFI_IP6) &&
       (safi == SAFI_ENCAP || safi == SAFI_MPLS_VPN))
     {
-        struct bgp_attr_encap_subtlv *pEncap;
-        unsigned int	              attrlenfield = 0;
 	/* Tunnel Encap attribute */
 	bgp_packet_mpattr_tea(bgp, peer, s, attr, BGP_ATTR_ENCAP);
-
-      /* compute attr length */
-      if (attr && attr->extra && attr->extra->encap_subtlvs) {
-	for (pEncap = attr->extra->encap_subtlvs; pEncap; pEncap = pEncap->next) {
-	  attrlenfield += (4 + pEncap->length);
-	}
-      }
-
-      if (attrlenfield) { /* only make attr if subtlvs exist */
-	  if (attrlenfield > 0xffff) {
-	    zlog (peer->log, LOG_ERR, 
-		"Tunnel Encap attribute is too long (length=%d), can't send it",
-		attrlenfield);
-	  } else {
-	    if (attrlenfield > 0xff) {
-	      /* 2-octet length field */
-	      stream_putc (s,
-		BGP_ATTR_FLAG_TRANS|BGP_ATTR_FLAG_OPTIONAL|BGP_ATTR_FLAG_EXTLEN);
-	      stream_putc (s, BGP_ATTR_ENCAP);
-	      stream_putw (s, attrlenfield & 0xffff);
-	    } else {
-	      /* 1-octet length field */
-	      stream_putc (s, BGP_ATTR_FLAG_TRANS|BGP_ATTR_FLAG_OPTIONAL);
-	      stream_putc (s, BGP_ATTR_ENCAP);
-	      stream_putc (s, attrlenfield & 0xff);
-	    }
-
-	    /* write each sub-tlv */
-	    for (pEncap = attr->extra->encap_subtlvs; pEncap; pEncap = pEncap->next) {
-	      stream_putw (s, pEncap->type);
-	      stream_putw (s, pEncap->length);
-	      stream_put (s, pEncap->value, pEncap->length);
-	    }
-	  }
-       }
     }
 
   /* Unknown transit attribute. */
